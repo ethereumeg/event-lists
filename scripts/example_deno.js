1cf4bb7f5f2c9b927a5d19b71562378d3535f2f5
tree <tree@hidden.cz>
2023-05-14 03:49:53 +0200

    update readme

--- scripts/example_deno.js
+++ scripts/example_deno.js
@@ -1,12 +1,12 @@
 import Ajv from "https://esm.sh/v114/ajv@8.12.0";
 import addFormats from "https://esm.sh/ajv-formats@2.1.1";
 import { parse as yamlParse } from "https://deno.land/std@0.187.0/yaml/mod.ts";
-import { schema } from "https://deno.land/x/event_lists@0.0.4-1/mod.ts";
+import { schema } from "https://deno.land/x/event_lists@0.0.4-2/mod.ts";
 
 const ajv = new Ajv({ strict: false });
 addFormats(ajv);
 const validate = ajv.compile(schema);
-/*
+
 const LIST_YAML_FILE = "./src/examples/minimal.yaml";
 
 const list = yamlParse(await Deno.readTextFile(LIST_YAML_FILE));
@@ -16,4 +16,3 @@ if (!validate(list)) {
 }
 
 console.log("Event List valid");
-*/
